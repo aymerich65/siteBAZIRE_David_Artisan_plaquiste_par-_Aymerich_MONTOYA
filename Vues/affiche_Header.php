7b0900c512654b2e785ff54262d7dbb4c8c5b30c
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-05 11:33:57 +0200

    Amélioration du header avec implémentation d'une image

--- Vues/affiche_Header.php
+++ Vues/affiche_Header.php
@@ -1,5 +1,8 @@
-<div class="row headerstyle"><h1 style="text-align:center">BAZIRE David<br><p class="undertitle">Artisan plaquiste</p></h1>
-            <div class="col">
-            </div>
+
+<div class="row headerstyle"><h1 style="text-align:center">BAZIRE David<br><p class="undertitle">Artisan plaquiste</p></h1> <img  src="../images/couteau_a_enduire2-PhotoRoom.png-PhotoRoom (1).png">
+
+<div class="headerdeco"></div>  
+           
+        </div>  
             
 
