471be519c37bddbeebd1c5537c8fd7154525da8f
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-22 11:20:28 +0200

    Correction du css de la page accueil

--- Vues/affiche_Header.php
+++ Vues/affiche_Header.php
@@ -1,10 +1,10 @@
 
 <div class="row headerstyle">
 
-        <h1 style="text-align:center">BAZIRE David<br><p class="undertitle">Artisan plaquiste</p></h1> <img  src="../images/couteau_a_enduire2-PhotoRoom.png-PhotoRoom (1).png">
+       
 
 <div class="headerdeco">
-
+<h1 style="text-align:center">BAZIRE David<br><p class="undertitle">Artisan plaquiste</p></h1> <img  src="../images/couteau_a_enduire2-PhotoRoom.png-PhotoRoom (1).png">
 
 </div>  
 
