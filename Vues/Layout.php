5ea5b3ae1f2072af9a5290b44233686580070a28
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-30 13:58:37 +0200

    test chemin css

--- Vues/Layout.php
+++ Vues/Layout.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-     <link rel="stylesheet" href="../CSS/bootstrap.css">
-    <link rel="stylesheet" href="../CSS/style.css">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+   <link rel="stylesheet" href="../CSS/bootstrap.css">
+    <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
-    <container class="container-fluid stylehomepage">
+    <container class="container-fluid">
 <header>
 <?php $header ?>
 </header>
