fff40e8b64b605bcb7926dbcfa4709bcdea6ae3a
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-30 14:02:18 +0200

    test de css pour résoudre le problème de non detection du fichier css

--- Vues/Layout.php
+++ Vues/Layout.php
@@ -9,6 +9,11 @@
    <link rel="stylesheet" href="../CSS/bootstrap.css">
     <link rel="stylesheet" href="../CSS/style.css">
 </head>
+<style>
+    body{
+        background-color : pink;
+    }
+    </style>
 <body>
     <container class="container-fluid">
 <header>
