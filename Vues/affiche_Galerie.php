a0579deb1874b9773a2950a1ef3b81645421fd8c
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-30 17:27:29 +0200

    galerie test

--- Vues/affiche_Galerie.php
+++ Vues/affiche_Galerie.php
@@ -1,7 +1,8 @@
 <?php
 ob_start();
 
-
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config.php';
 $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
              
              $myrequest = $pdo->prepare('SELECT * FROM galerie_images');
