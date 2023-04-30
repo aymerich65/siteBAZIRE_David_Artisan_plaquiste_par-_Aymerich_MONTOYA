70b56a1adf0999f17c2176765b4cf5ba325b6f70
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-30 18:54:52 +0200

    test

--- Vues/affiche_Galerie.php
+++ Vues/affiche_Galerie.php
@@ -1,5 +1,27 @@
 <?php
+/*
 
+
+
+
+ob_start();
+?>
+<div> <?php echo var_dump($mybddTable); ?></div>
+<?php
+$contenu = ob_get_clean();
+require_once 'Layout.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../config.php';
+$pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+$myrequest = $pdo->prepare('SELECT * FROM galerie_images');
+$myrequest->execute();
+$mybddTable = $myrequest->fetchAll(PDO::FETCH_ASSOC);  
+$imageFolder = '';             
+$myrequest = $pdo->prepare('SELECT * FROM galerie_images');
+$myrequest->execute();
+$mybddTable = $myrequest->fetchAll(PDO::FETCH_ASSOC);  
+$imageFolder = '';
+var_dump($mybddTable);*/
 $url = getenv('JAWSDB_URL');
 
 if ($url) {
