222eb2a29b9c58be483eaed0065518aa40ddb12c
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-04-30 17:41:45 +0200

    test galerie

--- Vues/affiche_Galerie.php
+++ Vues/affiche_Galerie.php
@@ -1,17 +1,19 @@
 <?php
-ob_start();
+/*
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config.php';
 $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
-             
+
              $myrequest = $pdo->prepare('SELECT * FROM galerie_images');
              $myrequest->execute();
              $mybddTable = $myrequest->fetchAll(PDO::FETCH_ASSOC);  
              $imageFolder = '';
-
+ob_start();
 ?>
 <div> <?php echo var_dump($mybddTable); ?></div>
 <?php
 $contenu = ob_get_clean();
-require_once 'Layout.php';
+require_once 'Layout.php';*/
+
+echo 'le test sans bdd';
