1bf2e2b8e6e7bc43ac28ba3293e928063f96f347
Aymerich MONTOYA <aymaman6576@gmail.com>
2023-05-01 09:29:03 +0200

    modification du fichier config

--- config.php
+++ config.php
@@ -5,27 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$dsn = $_ENV['DB_DSN'];
-if (!empty($dsn)) {
+$mode_prod = true;
 
 
-
-    $envuser = $_ENV['DB_USERNAME'];
-    $envpassword = $_ENV['DB_PASSWORD'];
-    $secret = $_ENV['SECRET'];
-    $token = $_ENV['TOKEN'];
-    $sendgridapikey = $_ENV['SENDGRID_API_KEY'];
-
-
-    define("DB_DSN", $dsn);
-    define("DB_USER", $envuser);
-    define("DB_PASSWORD", $envpassword);
-    define("SECRET", $secret);
-    define("TOKEN", $token);
-    define("SENDGRID_API_KEY", $sendgridapikey);
-} else {
+if ($mode_prod === true) {
 
     $url = getenv('JAWSDB_URL');
     $dbparts = parse_url($url);
@@ -35,6 +18,30 @@ if (!empty($dsn)) {
     $envpassword = $dbparts['pass'];
     $database = ltrim($dbparts['path'], '/');
     $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
+
+    define("DB_DSN", $dsn);
+    define("DB_USER", $envuser);
+    define("DB_PASSWORD", $envpassword);
+    define("SECRET", $secret);
+    define("TOKEN", $token);
+    define("SENDGRID_API_KEY", $sendgridapikey);
+} else {
+
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dsn = $_ENV['DB_DSN'];
+
+
+
+    
+
+    $envuser = $_ENV['DB_USERNAME'];
+    $envpassword = $_ENV['DB_PASSWORD'];
+    $secret = $_ENV['SECRET'];
+    $token = $_ENV['TOKEN'];
+    $sendgridapikey = $_ENV['SENDGRID_API_KEY'];
+
 
     define("DB_DSN", $dsn);
     define("DB_USER", $envuser);
